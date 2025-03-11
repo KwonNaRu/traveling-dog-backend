5031b101fe547d05891f8404b654e742086850eb
Naru <kwonnaru@kakao.com>
2025-03-11 09:57:08 +0900

    Update : 스웨거 로그인 인증 방식 변경
    - Authorize 버튼 눌러서 이메일, 패스워드 입력하고 로그인
    - 그 후 로그인 API를 try it 해서 로그인
    - 자동으로 쿠키 jwt token 적용
    - 다른 API 호출 가능

--- src/main/java/com/travelingdog/backend/config/SwaggerConfig.java
+++ src/main/java/com/travelingdog/backend/config/SwaggerConfig.java
@@ -1,5 +1,10 @@
 package com.travelingdog.backend.config;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -8,10 +13,6 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -31,21 +32,32 @@ public class SwaggerConfig {
                         .url("http://springdoc.org"));
 
         // JWT 인증 스키마 설정
-        SecurityScheme securityScheme = new SecurityScheme()
+        SecurityScheme jwtSecurityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER)
                 .name("Authorization");
 
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+        // Basic 인증 스키마 설정 추가
+        SecurityScheme basicSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("basic")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
 
+        // JWT 보안 요구사항
+        SecurityRequirement jwtSecurityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        // Basic 보안 요구사항은 전역으로 적용하지 않음 (로그인 API에만 적용)
         return new OpenAPI()
                 .info(info)
                 .servers(Arrays.asList(
                         new Server().url("/").description("현재 서버"),
                         new Server().url("https://api.travelingdog.com").description("운영 서버")))
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .addSecurityItem(securityRequirement);
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", jwtSecurityScheme)
+                        .addSecuritySchemes("basicAuth", basicSecurityScheme))
+                .addSecurityItem(jwtSecurityRequirement);
     }
 }
