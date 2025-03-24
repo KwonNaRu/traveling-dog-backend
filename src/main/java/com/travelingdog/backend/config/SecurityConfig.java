53036942184aa4bc8d830bb41647135cc82591e8
Naru <kwonnaru@kakao.com>
2025-03-24 13:23:50 +0900

    - SecurityConfig 클래스에서 CORS 설정을 업데이트하여 허용된 출처 패턴을 개선 - localhost와 narudog.com의 서브도메인을 포함하도록 변경.

--- src/main/java/com/travelingdog/backend/config/SecurityConfig.java
+++ src/main/java/com/travelingdog/backend/config/SecurityConfig.java
@@ -59,10 +59,7 @@ public class SecurityConfig {
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(Arrays.asList(
-                                "http://localhost:3000",
-                                "https://traveling-dev.narudog.com",
-                                "https://traveling.narudog.com"));
+                configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://*.narudog.com"));
 
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 
